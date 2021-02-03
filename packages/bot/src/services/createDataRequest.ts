48f86381dc082b7f88e8f170373c438f9f76bc9c
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2021-02-03 17:40:55 +0100

    feat(bot): change data request to return map

--- packages/bot/src/services/createDataRequest.ts
+++ packages/bot/src/services/createDataRequest.ts
@@ -6,7 +6,7 @@ export function createDataRequest (channelId: string, messageId: string) {
   const url = baseUrl + '?channelId=' + channelId + '&messageId=' + messageId
 
   // Use this JSON as a template
-  const requestJson = `{"jsonrpc":"2.0","method":"sendRequest","id":"1","params":{"dro":{"data_request":{"time_lock":0,"retrieve":[{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_1","script":[130, 24, 119, 130, 24, 101, 104, 112, 111, 115, 105, 116, 105, 118, 101]},{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_2","script":[130, 24, 119, 130, 24, 101, 104, 112, 111, 115, 105, 116, 105, 118, 101]},{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_3","script":[130, 24, 119, 130, 24, 101, 104, 112, 111, 115, 105, 116, 105, 118, 101]}],"aggregate":{"filters":[],"reducer":2},"tally":{"filters":[{"op":8,"args":[]}],"reducer":2}},"witness_reward":1000,"witnesses":3,"commit_and_reveal_fee":10,"min_consensus_percentage":51,"collateral":1000000000},"fee":0}}`
+  const requestJson = `{"jsonrpc":"2.0","method":"sendRequest","id":"1","params":{"dro":{"data_request":{"time_lock":0,"retrieve":[{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_1","script":[129, 24, 119]},{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_2","script":[129, 24, 119]},{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_3","script":[129, 24, 119]}],"aggregate":{"filters":[],"reducer":2},"tally":{"filters":[{"op":8,"args":[]}],"reducer":2}},"witness_reward":1000,"witnesses":3,"commit_and_reveal_fee":10,"min_consensus_percentage":51,"collateral":1000000000},"fee":0}}`
   const request = JSON.parse(requestJson)
 
   // Modify url of each script
