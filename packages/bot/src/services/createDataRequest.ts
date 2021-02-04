75d725ee239362ec15d28c3e6fe2408735c0a179
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2021-02-04 10:57:46 +0100

    fix(bot): change URL parameters to snake_case

--- packages/bot/src/services/createDataRequest.ts
+++ packages/bot/src/services/createDataRequest.ts
@@ -3,7 +3,7 @@ export function createDataRequest (channelId: string, messageId: string) {
   // TODO: set middleware URL here:
   const baseUrl = 'http://docker.witnet.io:3000/'
   // TODO: channelId and messageId are assumed to be valid URL parameters
-  const url = baseUrl + '?channelId=' + channelId + '&messageId=' + messageId
+  const url = baseUrl + '?channel_id=' + channelId + '&message_id=' + messageId
 
   // Use this JSON as a template
   const requestJson = `{"jsonrpc":"2.0","method":"sendRequest","id":"1","params":{"dro":{"data_request":{"time_lock":0,"retrieve":[{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_1","script":[129, 24, 119]},{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_2","script":[129, 24, 119]},{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_3","script":[129, 24, 119]}],"aggregate":{"filters":[],"reducer":2},"tally":{"filters":[{"op":8,"args":[]}],"reducer":2}},"witness_reward":1000,"witnesses":3,"commit_and_reveal_fee":10,"min_consensus_percentage":51,"collateral":1000000000},"fee":0}}`
