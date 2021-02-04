36b8352fae9c8fa09dc3ef904ca98f386362b5b8
María Gabaldón Rico <mariag@stampery.co>
2021-02-04 19:22:54 +0100

    feat(bot): execute proposal and send message to client

---
+++ packages/bot/src/services/executeVotingResult.ts
@@ -0,0 +1,10 @@
+import { Web3Client } from './web3Client'
+
+const web3 = new Web3Client()
+
+export async function executeVotingResult (dao: any, payload: any) {
+  return web3.execute(
+    dao,
+    payload,
+  )
+}
