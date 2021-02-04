be540791370c6c34dcdf4dfe788f3f81299f318a
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2021-02-04 16:20:09 +0100

    docs(bot): explain how to generate the data request
    Co-authored-by: Luis Rubio <l.rubiorod@gmail.com>

--- packages/bot/src/services/createDataRequest.ts
+++ packages/bot/src/services/createDataRequest.ts
@@ -6,6 +6,16 @@ export function createDataRequest (channelId: string, messageId: string) {
   const url = baseUrl + '?channel_id=' + channelId + '&message_id=' + messageId
 
   // Use this JSON as a template
+  // Use the following Rust code to generate the script:
+  // https://github.com/lrubiorod/rad-script
+  /*
+    pub fn dr() -> Vec<u8> {
+        cbor_to_vec(&Value::Array(vec![
+            Value::Integer(RadonOpCodes::StringParseJSONMap as i128),
+        ]))
+        .unwrap()
+    }
+  */
   const requestJson = `{"jsonrpc":"2.0","method":"sendRequest","id":"1","params":{"dro":{"data_request":{"time_lock":0,"retrieve":[{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_1","script":[129, 24, 119]},{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_2","script":[129, 24, 119]},{"kind":"HTTP-GET","url":"PLACEHOLDER_URL_3","script":[129, 24, 119]}],"aggregate":{"filters":[],"reducer":2},"tally":{"filters":[{"op":8,"args":[]}],"reducer":2}},"witness_reward":1000,"witnesses":3,"commit_and_reveal_fee":10,"min_consensus_percentage":51,"collateral":1000000000},"fee":0}}`
   const request = JSON.parse(requestJson)
 
