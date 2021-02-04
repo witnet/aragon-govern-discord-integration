35e1eeaf75c8f886dbf1abd0820d10b21b55c389
Adán SDPC <adan.sdpc@gmail.com>
2021-02-04 16:57:49 +0100

    feat(bot): only parse commands if used in front of the message

--- packages/bot/src/services/commandFinder.ts
+++ packages/bot/src/services/commandFinder.ts
@@ -4,14 +4,14 @@ import { Command } from '../types'
 @injectable()
 export class CommandFinder {
   public isNewDaoMessage (message: string) {
-    return message.search(Command.NewDao) >= 0
+    return message.search(Command.NewDao) == 0
   }
 
   public isNewProposalMessage (message: string) {
-    return message.search(Command.NewProposal) >= 0
+    return message.search(Command.NewProposal) == 0
   }
 
   public isSetupMessage (message: string) {
-    return message.search(Command.Setup) >= 0
+    return message.search(Command.Setup) == 0
   }
 }
