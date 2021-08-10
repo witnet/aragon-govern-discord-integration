7783669946fec34fd70e604eee8a2aacf6eae85a
Mario Cao <mariocao@gmail.com>
2021-08-10 11:39:14 +0200

    chore(bot): change default Witnet node port to 21338

--- packages/bot/src/config.ts
+++ packages/bot/src/config.ts
@@ -1,5 +1,5 @@
 export const WITNET_NODE_HOST = process.env.WITNET_NODE_HOST || '127.0.0.1'
 export const ENVIRONMENT = process.env.ENVIRONMENT || 'production'
-export const WITNET_NODE_PORT = parseInt(process.env.WITNET_NODE_PORT || '1234')
+export const WITNET_NODE_PORT = parseInt(process.env.WITNET_NODE_PORT || '21338')
 export const DEFAULT_DB_PATH = './bot.db'
 export const DEFAULT_EXEC_TIME = 120
