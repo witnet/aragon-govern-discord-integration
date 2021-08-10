f0ed1f4356dd0b8e9c61fe0a00d257e0d70b944b
Mario Cao <mariocao@gmail.com>
2021-08-10 11:53:03 +0200

    chore(docker): update downloader

--- docker/downloader.sh
+++ docker/downloader.sh
@@ -19,18 +19,18 @@ AUTH="Authorization: token $GITHUB_API_TOKEN"
 
 # If version is "latest", resolve the latest version number
 if [[ "$VERSION" == "latest" ]]; then
-    VERSION=$(curl --cacert /etc/ssl/certs/ca-certificates.crt -H "$AUTH" https://api.github.com/repos/stampery-labs/aragon-govern-discord-integration/releases/latest | sed -En "s/.*\"tag_name\": \"([0-9|\.]+)\",/\1/p" | head -1)
+    VERSION=$(curl --cacert /etc/ssl/certs/ca-certificates.crt -H "$AUTH" https://api.github.com/repos/witnet/aragon-govern-discord-integration/releases/latest | sed -En "s/.*\"tag_name\": \"([0-9|\.]+)\",/\1/p" | head -1)
 fi
 
 # The URL of the release file, and the name that will be used when downloaded
-URL="https://api.github.com/repos/stampery-labs/aragon-govern-discord-integration/tarball/$VERSION"
+URL="https://api.github.com/repos/witnet/aragon-govern-discord-integration/tarball/$VERSION"
 FILENAME="release.tar.gz"
 
 # Download and extract release bundle
 log "Downloading version $VERSION ('$FILENAME') from '$URL'. This may take a few seconds..."
 curl --cacert /etc/ssl/certs/ca-certificates.crt -LH "$AUTH" -o "/tmp/$FILENAME" "$URL" &&
 tar -zxf "/tmp/$FILENAME" --directory "/tmp/" &&
-mv /tmp/stampery-labs-aragon-govern-discord-integration-* /data/source &&
+mv /tmp/witnet-aragon-govern-discord-integration-* /data/source &&
 # Clean after ourselves
 rm -f "/tmp/$FILENAME" &&
 # Show success or error message
